e87d0ea369ba88b664b91349bc03f52fa6365162
hossein khosravi <mhosseink1380@gmail.com>
2022-12-07 17:50:37 +0330

    reinstall packages

--- src/components/appointment/Result.js
+++ src/components/appointment/Result.js
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { toPersianNum, convertDate, convertISOS } from "../../tools/helper";
 import { ResultStyle } from "./styleAppoinment";
 
-import { useScreenshot, createFileName } from "use-react-screenshot";
+import { /* useScreenshot, */ createFileName } from "use-react-screenshot";
 import { useTranslation } from "react-i18next";
 
 const Result = () => {
@@ -12,17 +12,17 @@ const Result = () => {
   const lang = useSelector((state) => state.stateLang.lng);
   const { t } = useTranslation();
   const ref = createRef(null);
-  const [image, takeScreenShot] = useScreenshot({
+ /*  const [image, takeScreenShot] = useScreenshot({
     type: "image/jpeg",
     quality: 1.0
-  });
+  }); */
   const download = (image, { name = "img", extension = "jpg" } = {}) => {
     const a = document.createElement("a");
     a.href = image;
     a.download = createFileName(extension, name);
     a.click();
   };
-  const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
+  /* const downloadScreenshot = () => takeScreenShot(ref.current).then(download); */
   return (
     <>
       <ResultStyle ref={ref}>
@@ -63,7 +63,10 @@ const Result = () => {
           <p>{t("ruleText")}</p>
         </div>
       </ResultStyle>
-      <button className="saveGallery submitBtn" onClick={downloadScreenshot}>
+   {/*    <button className="saveGallery submitBtn" onClick={downloadScreenshot}>
+        {t("SavetoGallery")}
+      </button> */}
+      <button className="saveGallery submitBtn">
         {t("SavetoGallery")}
       </button>
     </>
